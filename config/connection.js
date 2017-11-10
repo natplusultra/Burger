2345da1f3d0ee6eac65926030c98f494bc295c04
Natalie Loman <natalie4@gmail.com>
2017-11-10 12:19:02 -0800

    updated connection config with JawsDB code

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // dependencies
 var mysql = require("mysql");
+var connection;
 
-// configures the connection between node and mysql
-var connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "burgers_db"
-});
+// uses JawsDB to configure connection
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "",
+		database: "burgers_db"
+	});
+}
 
 connection.connect(function(err) {
 	if (err) {
