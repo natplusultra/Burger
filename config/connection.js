78da7f8d4cdc19c67e5d430d57ade9bf53d2d405
Natalie Loman <natalie4@gmail.com>
2017-11-14 15:09:48 -0800

    updated config

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,7 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
+		root: 3000,
 		host: "localhost",
 		user: "root",
 		password: "",
